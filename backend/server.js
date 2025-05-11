8ff0e43c6107f950f3226d866a875d6dab3663e8
sugunasri <svvsssmedia@gmail.com>
2025-05-11 21:38:33 +0530

    Added authRoutes

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
-const checkEnv = require("./config/intialCheck");
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
