e2a49b06d863074b37812c58d28374c89dd48b63
sugunasri <svvsssmedia@gmail.com>
2025-05-12 13:33:12 +0530

    Added Report  Routes

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const taskRoutes = require("./routes/taskRoutes");
+const reportRoutes = require("./routes/reportRoutes");
 
 const app = express();
 
@@ -30,7 +31,7 @@ connectDB();
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/tasks", taskRoutes);
-// app.use("/api/reports", reportRoutes);
+app.use("/api/reports", reportRoutes);
 
 // Start Server
 
