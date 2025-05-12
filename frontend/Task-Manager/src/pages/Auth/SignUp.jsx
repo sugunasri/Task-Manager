e5230cf9033abbd85c5a4cf856639729157e6f41
sugunasri <svvsssmedia@gmail.com>
2025-05-12 22:22:57 +0530

    Created LogIn UI

--- frontend/Task-Manager/src/pages/Auth/SignUp.jsx
+++ frontend/Task-Manager/src/pages/Auth/SignUp.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AuthLayout from '../../components/layouts/AuthLayout'
+import { useAsyncError } from 'react-router-dom';
 
 const SignUp = () => {
+  const [ profilePic, setProfile ] = useState(null);
+  const [ fullName, setFullName ] = useState("");
+  const [ email, setEmail ] = useState("");
+  const [ password, setPassword ] = useState("");
+  const [ adminInviteToken, setAdminInviteToken] = useState('');
+
+  const [ error, setError ] = useState("");
   return (
-    <div>
-      SignUp
-    </div>
+    <AuthLayout>
+
+    </AuthLayout>
   )
 }
 
